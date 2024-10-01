001f6600b8d954bf892a601e391ff3735adb7a26
Fernando Peralta <73323113+fpera0248@users.noreply.github.com>
2024-10-01 07:44:30 -0400

    Time: 36 ms (79.56%) | Memory: 17.8 MB (5.76%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -6,11 +6,77 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
+        '''
+        U:
+        track longest path from root to farthest leaf
+        including root node in total count
+
+        bad case:
+        empty tree - return 0
+
+
+        M:
+        binary tree traversal, dfs
+
+
+        P:
+        iterate through each level of the bianry tree, tracking the longest path 
+                    3
+                   / \
+                  9  20 
+                    /  \
+                   15   7
+                     \
+                      6
+
+        iterative approach: traverse through left side of tree, if leaf node(no children) move back up and review next node on left side, track the longest path
+        do the same for right side
+
+        recursive approach: create function for recursive tranversal, iterate through each side, find a way to collect and track teh longest path for leaf nodes  
+        out put int of longest path
         
+        I:
+        '''
         if not root:
             return 0
 
+        #iterative
+      #  while root.left:
+            
+
+
+        count = 1
+        res = []
+
+        #recursive
         left = self.maxDepth(root.left)
         right = self.maxDepth(root.right)
 
+
+        # if root.left == None and root.right == None:
+        #     res.append(count)
+        #     count -= 1
+        # else:
+        #     count += 1
+
+
+        # for i in self.maxDepth(root.right):
+        #     if root.left == None and root.right == None:
+        #         res.append(count)
+        #         count -= 1
+        #     else:
+        #         count += 1
+
         return max(left, right) + 1
+
+
+
+        '''    
+        R:
+
+
+
+        E:
+
+
+        '''
