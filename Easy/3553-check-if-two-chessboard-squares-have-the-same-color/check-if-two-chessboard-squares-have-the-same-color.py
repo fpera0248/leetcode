dd9181947a7f7e04e9effd19ef8db903f1daad28
Fernando Peralta Castro <73323113+fpera0248@users.noreply.github.com>
2025-03-07 06:40:49 -0500

    Time: 44 ms (22.60%) | Memory: 18.1 MB (16.54%) - LeetSync

--- Easy/3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.py
+++ Easy/3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.py
@@ -1,9 +1,16 @@
 class Solution:
     def checkTwoChessboards(self, coordinate1: str, coordinate2: str) -> bool:
-        # Convert a coordinate to its color (True for white, False for black)
-        def is_white(coordinate: str) -> bool:
-            col = ord(coordinate[0]) - ord('a') + 1  # Convert 'a'-'h' to 1-8
-            row = int(coordinate[1])
-            return (col + row) % 2 == 0
         
-        return is_white(coordinate1) == is_white(coordinate2)
+        col1 = ord(coordinate1[0]) - ord('a') + 1
+        col2 = ord(coordinate2[0]) - ord('a') + 1
+
+        row1 = int(coordinate1[1])
+        row2 = int(coordinate2[1])
+
+        res1 = (col1 + row1)
+        res2 = (col2 + row2)
+
+        print(res1, res2)
+
+        return (res1 % 2) == (res2 % 2)
+            
