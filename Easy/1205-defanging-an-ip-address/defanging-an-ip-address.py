ef390699ff33c18c359330f47edc36c5fbc471d2
Fernando Peralta Castro <73323113+fpera0248@users.noreply.github.com>
2025-08-01 19:22:32 -0400

    Time: 35 ms (72.25%) | Memory: 17.6 MB (61.94%) - LeetSync

---
+++ Easy/1205-defanging-an-ip-address/defanging-an-ip-address.py
@@ -0,0 +1,34 @@
+class Solution:
+    def defangIPaddr(self, address: str) -> str:    
+        '''
+        # U
+        Claryfiying questions
+        Edge cases
+
+        # M
+        data structures
+
+        # P
+        pseucode, how to solve step by step
+
+        # I
+        actually doing the problem
+
+        # R
+        why it works
+
+        # E
+        time coompletxity and space complexity
+
+        1.1.1.1    
+        '''
+        # new_address = ''
+
+        # for i in address:
+        #     if i == '.':
+
+        new_address = address.replace('.', '[.]')
+        return new_address
+                
+                
+
