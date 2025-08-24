97cc53d58d8670b4aeb468156b4808785b46452b
Fernando Peralta Castro <73323113+fpera0248@users.noreply.github.com>
2025-08-24 18:59:46 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (52.02%) - LeetSync

--- Easy/782-jewels-and-stones/jewels-and-stones.py
+++ Easy/782-jewels-and-stones/jewels-and-stones.py
@@ -1,10 +1,28 @@
+
+
+'''
+
+strings jewels : stones that are jewels
+
+stones: stones i have
+
+each cahracter in tones is a tone you have
+
+find how mnay stone are also jewels
+
+plan:
+
+use jewels to track how many times each index occurs in stones, and add counter for each time then return counter
+'''
+
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         
-        res = set(jewels)
-        output = 0
+        res = 0
+        jewels_new = set(jewels)
 
         for i in stones:
-            if i in res:
-                output += 1
-        return output
+            if i in jewels_new:
+                res += 1
+        return res
+
