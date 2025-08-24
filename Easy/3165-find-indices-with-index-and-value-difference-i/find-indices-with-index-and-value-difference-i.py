f90ad63990324419cff935318f0d2d59c0e91d3a
Fernando Peralta Castro <73323113+fpera0248@users.noreply.github.com>
2025-08-24 18:28:08 -0400

    Time: 4 ms (18.77%) | Memory: 17.9 MB (37.54%) - LeetSync

--- Easy/3165-find-indices-with-index-and-value-difference-i/find-indices-with-index-and-value-difference-i.py
+++ Easy/3165-find-indices-with-index-and-value-difference-i/find-indices-with-index-and-value-difference-i.py
@@ -1,8 +1,26 @@
+'''
+
+U: 
+
+i and j, that satifies both condiditions
+
+return arr answer, where i,j are values 
+
+if two indices cant match these conditions return [-1,-1]
+
+P:
+
+double for loop, iterate through list, if conditions are meet , add i and j to answer and return
+
+if not [-1,-1]
+'''
 class Solution:
     def findIndices(self, nums: List[int], indexDifference: int, valueDifference: int) -> List[int]:
-        n = len(nums)
-        for i in range(n):
-            for j in range(i, n):  # Changed from i+1 to i
+        answer = []
+
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+
                 if abs(i - j) >= indexDifference and abs(nums[i] - nums[j]) >= valueDifference:
-                    return [i, j]
+                    return [i,j]
         return [-1, -1]
