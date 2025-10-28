ad97e3a7f70c09c503ad590bf64e65cd66cf85a9
Fernando Peralta Castro <73323113+fpera0248@users.noreply.github.com>
2025-10-28 15:06:43 -0400

    Time: 131 ms (25.44%) | Memory: 26.9 MB (77.79%) - LeetSync

--- Easy/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ Easy/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        l, r = 0, 1
-        maxP = 0
-
-        while r < len(prices):
-            if prices[l] < prices[r]:
-                profit = prices[r] - prices[l]
-                maxP = max(maxP, profit)
-            else:
-                l = r
-            r += 1
-        return maxP
+        min_price = float('inf')
+        max_profit = 0
+        
+        for price in prices:
+            min_price = min(min_price, price)          # buy low
+            max_profit = max(max_profit, price - min_price)  # sell high
+        return max_profit
