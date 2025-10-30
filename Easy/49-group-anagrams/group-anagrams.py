eb30bd31d1ae739b30d4477d67a2fe3f9100d3f4
Fernando Peralta Castro <73323113+fpera0248@users.noreply.github.com>
2025-10-30 14:25:51 -0400

    Time: 40 ms (7.59%) | Memory: 25.4 MB (11.64%) - LeetSync

--- Easy/49-group-anagrams/group-anagrams.py
+++ Easy/49-group-anagrams/group-anagrams.py
@@ -1,17 +1,15 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        helper = {}
-        for st in strs:
-             sortedVersion  = sorted(st)
-             temp = ""
-             for ele in sortedVersion:
-                 temp = temp + ele
-             sortedVersion = temp     
-             if sortedVersion in helper:
-                 helper[sortedVersion].append(st)       
-             else:
-                helper[sortedVersion] = [st]
-        print(helper)
-        return [ele for ele in helper.values()]               
-                
+        hashmap = {}
+        res = []
 
+        for s in strs:
+            key = tuple(sorted(Counter(s).items()))
+            if key not in hashmap:
+                hashmap[key] = []
+            hashmap[key].append(s)
+
+        for group in hashmap.values():
+            res.append(group)
+
+        return res
